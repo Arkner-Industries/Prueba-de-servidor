948a1659f592bda5459c692f5fdb6c643f15460f
Arkner-Industries <hornesfer@gmail.com>
2026-06-11 11:47:17 -0300

    correccion_errores

--- config.js
+++ config.js
@@ -1,29 +1,34 @@
 import mysql from "mysql2/promise"
 import express from "express"
+import hbs from "hbs"
 import { fileURLToPath } from "url"
 import { dirname } from "path"
-import hbs from "hbs"
+import { config } from "dotenv"
+
+config()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-import { config } from "dotenv"
-const ruta = `${__filename}/view`
+const ruta = `${__dirname}/views`
+const router = express.Router()
 
-const servidor = express
+
+const servidor = express()
 servidor.listen(4000)
 
 servidor.use(express.static(ruta))
 servidor.use(express.json())
-servidor.set("view engine","hbs")
-const router = express.Router()
+servidor.set("view engine", "hbs")
 hbs.registerPartials(`${__dirname}/views/partials`)
 
-config()
+
+
 try{
     const connection = await mysql.createConnection(
-        `mysql://${procces.env.usuario}:${process.env.contra}@localhost:3306/${process.env.db}`
+        `mysql://${process.env.usuario}:${process.env.contra}@localhost:3306/${process.env.db}`
     );
     await connection.end();
+
 }catch (err){
     console.log(err);
 }
@@ -31,10 +36,8 @@ try{
 servidor.use(router)
 
 export{
-    router,
+    servidor,
     ruta,
-    servidor
+    router
 }
-
-
 
--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-import { router, ruta, servidor } from "./config"
+import { servidor, router, ruta } from "./config.js"
 
+
+router.get("/",(req,res)=>{
+    res.render("login.hbs")   
+})
+
+
+router.get("/login",(req,res)=>{
+    res.render("login.hbs")
+})
+
+router.post("/login",(req,res)=>{
+    console.log(req.body)
+    res.send("soriegueton")
+})
 
